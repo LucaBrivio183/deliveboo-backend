f8372e389c34f00ae03127879ca0986cb70b04f0
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-06-15 10:13:01 +0200

    ProductSeeder generated by config file

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
@@ -15,26 +14,24 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
-        $products = ['Pizza margherita', 'Patatine fritte', 'Hamburger classico', 'Insalatina fresca', 'Hot dog'];
-        $restaurant = 1;
+        $products = config('products');
 
         foreach ($products as $product) {
 
             $newProduct = new Product();
 
-            $newProduct->restaurant_id = $restaurant;
-            $newProduct->name = $product;
+            $newProduct->restaurant_id = $product['restaurant_id'];
+            $newProduct->name = $product['name'];
             $newProduct->slug = Str::slug($newProduct->name, '-');
-            $newProduct->description = 'Buonissimo, diffidate dalle imitazioni!';
-            $newProduct->ingredients = 'Quello che avevamo in cucina';
-            $newProduct->image = $faker->imageUrl(360, 360, 'food', true);
-            $newProduct->price = $faker->randomFloat(2, 1, 20);
+            $newProduct->description = $product['description'];
+            $newProduct->ingredients = $product['ingredients'];
+            $newProduct->image = $product['image'];
+            $newProduct->price = $product['price'];
             $newProduct->discount = 0;
             $newProduct->is_visible = 1;
             
-            $restaurant++;
             $newProduct->save();
 
         }
