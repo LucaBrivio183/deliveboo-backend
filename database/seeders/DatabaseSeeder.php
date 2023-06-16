c6f4384338f42c282375c2f66513675771fec3ff
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-06-16 20:05:22 +0200

    removed comment from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //UserSeeder::class,
+            UserSeeder::class,
             RestaurantSeeder::class,
             ProductSeeder::class,
             CategorySeeder::class,
