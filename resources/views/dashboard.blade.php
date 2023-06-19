0a8c8a51b821440b9802a4f6f5065c569be15507
eugeniarossi <eugenia.rossi@outlook.com>
2023-06-20 01:38:38 +0200

    fix: dashboard background

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,69 +1,70 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container" id="dashboard-container">
-    <!--<img src="{{url('/logo-green.png')}}" alt="logo" id="logo" id="background-image">-->
-    <h2 class="fs-4 my-4">
-        {{ __('Dashboard utente') }}
-    </h2>
-    <div class="row justify-content-center align-items-stretch">
-        <div class="col-3">
-            <div class="card my-3" id="restaurant-container">
-                <!--<h3 class="fs-4 my-4">
-                    {{ __('Il tuo ristorante') }}
-                </h3>-->
-                <div class="card-body d-flex flex-wrap justify-content-center align-items-center gap-3">
-                    @foreach ($restaurants as $restaurant)
-                    <a href="{{ route('admin.restaurants.show', $restaurant->id)}}">
-                        <div class="card">
-                            <img :src={{$restaurant->image}} class="card-img-top" :alt={{$restaurant->name}}>
-                            <div class="card-body">
-                                <h5 class="card-title">{{$restaurant->name}}</h5>
-                                <span>Indirizzo: {{$restaurant->address}},</span>
-                                <span>{{$restaurant->city}}</span>
-                                <div>CAP: {{$restaurant->postal_code}},</div>
-                                <div>Telefono: +{{$restaurant->phone_number}}</div>
-                                <div>Orari: {{$restaurant->business_times}}</div>
-                                <span class="badge bg-secondary"></span></h6>
+<div id="dashboard-container">
+    <div class="container">
+        <h2 class="fs-4 my-4">
+            {{ __('Dashboard utente') }}
+        </h2>
+        <div class="row justify-content-center align-items-stretch">
+            <div class="col-3">
+                <div class="card my-3" id="restaurant-container">
+                    <h3 class="fs-4 my-4 ms-4">
+                        {{ __('Il tuo ristorante') }}
+                    </h3>
+                    <div class="card-body d-flex flex-wrap justify-content-center align-items-center gap-3">
+                        @foreach ($restaurants as $restaurant)
+                        <a href="{{ route('admin.restaurants.show', $restaurant->id)}}">
+                            <div class="card">
+                                <img :src={{$restaurant->image}} class="card-img-top" :alt={{$restaurant->name}}>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{$restaurant->name}}</h5>
+                                    <span>Indirizzo: {{$restaurant->address}},</span>
+                                    <span>{{$restaurant->city}}</span>
+                                    <div>CAP: {{$restaurant->postal_code}},</div>
+                                    <div>Telefono: +{{$restaurant->phone_number}}</div>
+                                    <div>Orari: {{$restaurant->business_times}}</div>
+                                    <span class="badge bg-secondary"></span></h6>
+                                </div>
                             </div>
-                        </div>
-                    </a>
-                    @endforeach       
-                </div>
-            </div> 
-        </div>
-        <div class="col-9">
-            <div class="card my-3" id="products-container">
-                <!--<h3 class="fs-4 my-4 mx-6">
-                    {{ __('I tuoi prodotti') }}
-                </h3>-->
-                <div class="card-body d-flex flex-wrap justify-content-center align-items-center gap-3">
-                    {{-- element to repeat --}}
-                    @foreach ($products as $product)
-                    {{-- card --}}
-                    <a href="{{ route('admin.products.show', $product) }}">
-                        <div class="card" style="width: 18rem;">
-                            {{-- image --}}
-                            <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->name }}">
-                            <div class="card-body">
-                                {{-- name --}}
-                                <h5 class="card-title">{{ $product->name }}</h5>
-                            </div> 
-                        </div>
-                    </a>
-                    @endforeach
-                    {{-- element to repeat --}}
-    
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+                        </a>
+                        @endforeach       
                     </div>
-                    @endif
-                    {{-- {{ __('You are logged in!') }} --}}
+                </div> 
+            </div>
+            <div class="col-9">
+                <div class="card my-3" id="products-container">
+                    <h3 class="fs-4 my-4 ms-5">
+                        {{ __('I tuoi prodotti') }}
+                    </h3>
+                    <div class="card-body d-flex flex-wrap justify-content-center align-items-center gap-3">
+                        {{-- element to repeat --}}
+                        @foreach ($products as $product)
+                        {{-- card --}}
+                        <a href="{{ route('admin.products.show', $product) }}">
+                            <div class="card" style="width: 18rem;">
+                                {{-- image --}}
+                                <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->name }}">
+                                <div class="card-body">
+                                    {{-- name --}}
+                                    <h5 class="card-title">{{ $product->name }}</h5>
+                                </div> 
+                            </div>
+                        </a>
+                        @endforeach
+                        {{-- element to repeat --}}
+        
+                        @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                        @endif
+                        {{-- {{ __('You are logged in!') }} --}}
+                    </div>
                 </div>
             </div>
+            
         </div>
-        
     </div>
 </div>
 @endsection
