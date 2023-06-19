b79b1147d9ae91cc63f27f2266ed0ff53e235eb0
eugeniarossi <eugenia.rossi@outlook.com>
2023-06-19 17:24:09 +0200

    feat: logo and dashboard style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,45 +1,32 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
+<div class="container" id="dashboard-container">
+    <!--<img src="{{url('/logo-green.png')}}" alt="logo" id="logo" id="background-image">-->
+    <h2 class="fs-4 my-4">
         {{ __('Dashboard utente') }}
     </h2>
     <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('Il tuo ristorante') }}</div>
-                <div class="card-body d-flex flex-wrap justify-content-center align-items-center gap-3">
-                    @foreach ($restaurants as $restaurant)
-                    <a href="{{ route('admin.restaurants.show', $restaurant->id)}}">
-                        <div class="card">
-                            <img :src={{$restaurant->image}} class="card-img-top" :alt={{$restaurant->name}}>
-                            <div class="card-body">
-                                <h5 class="card-title">{{$restaurant->name}}</h5>
-                                <span>Indirizzo: {{$restaurant->address}},</span>
-                                <span>{{$restaurant->city}}</span>
-                                <div>CAP: {{$restaurant->postal_code}},</div>
-                                <div>Telefono: +{{$restaurant->phone_number}}</div>
-                                <div>Orari: {{$restaurant->business_times}}</div>
-                                <span class="badge bg-secondary"></span></h6>
-                            </div>
-                        </div>
-                    </a>
-                    @endforeach
-
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+        <div class="col-3">
+            @foreach ($restaurants as $restaurant)
+            <a href="{{ route('admin.restaurants.show', $restaurant->id)}}">
+                <div class="card">
+                    <img :src={{$restaurant->image}} class="card-img-top" :alt={{$restaurant->name}}>
+                    <div class="card-body">
+                        <h5 class="card-title">{{$restaurant->name}}</h5>
+                        <span>Indirizzo: {{$restaurant->address}},</span>
+                        <span>{{$restaurant->city}}</span>
+                        <div>CAP: {{$restaurant->postal_code}},</div>
+                        <div>Telefono: +{{$restaurant->phone_number}}</div>
+                        <div>Orari: {{$restaurant->business_times}}</div>
+                        <span class="badge bg-secondary"></span></h6>
                     </div>
-                    @endif
-                    {{-- {{ __('You are logged in!') }} --}}
-
                 </div>
-            </div>
-
+            </a>
+            @endforeach        
+        </div>
+        <div class="col-7">
             <div class="card my-3">
-                <div class="card-header">{{ __('I tuoi prodotti') }}</div>
-
                 <div class="card-body d-flex flex-wrap justify-content-center align-items-center gap-3">
                     {{-- element to repeat --}}
                     @foreach ($products as $product)
@@ -56,7 +43,7 @@
                     </a>
                     @endforeach
                     {{-- element to repeat --}}
-
+    
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
@@ -66,6 +53,7 @@
                 </div>
             </div>
         </div>
+        
     </div>
 </div>
 @endsection
