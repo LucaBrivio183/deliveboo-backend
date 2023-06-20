a2959c59eeca5d5be335f9595a13a43d102a6900
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-06-20 16:10:45 +0200

    feat: restaurant-typologies-api

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{   
+
+    // Get all restaurants (paginated by 5) with their typologies
+    public function index() {
+
+        try {
+
+            $restaurants = Restaurant::with('typologies')->paginate(5);
+            
+            if($restaurants) {
+    
+                return response()->json([
+                    'success' => true,
+                    'results' => $restaurants,
+                ]);
+            } else {
+    
+                return response()->json([
+                    'success' => false,
+                    'results' => null
+                ], 404);
+            }
+            
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'results' => null
+            ], 500);
+        }
+    }
+}
+
