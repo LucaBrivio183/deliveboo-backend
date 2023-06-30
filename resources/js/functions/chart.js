6e4a9638c4de50cafbc9c43cc63e7d68b9893e7c
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-06-30 20:55:12 +0200

    money chart created

--- resources/js/functions/chart.js
+++ resources/js/functions/chart.js
@@ -1,23 +1,36 @@
 import { Chart } from 'chart.js/auto';
 window.Chart = Chart;
 
-console.log(orders);
 // Months names
 const months = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'];
 // Number of orders for each month
 let ordersNumber = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// Money gained each month
+let moneyGained = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// Find the order creation month
+function findOrderCreationMonth(order) {
+    const date = new Date(order.created_at);
+    const month = date.getMonth();
+    return month;
+}
 
 // The script should be triggered at DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function () {
 
-    // For each order find the creation month
-    // and then increase the corresponding number into the given array
+    // Foreach order increase the corresponding number into the given array
     orders.forEach(order => {
-        const date = new Date(order.created_at);
-        const month = date.getMonth();
+        const month = findOrderCreationMonth(order);
         ordersNumber[month] += 1;
     });
 
+    orders.forEach(order => {
+        const month = findOrderCreationMonth(order);
+        const money = Number(order.total_price);
+        moneyGained[month] += money;
+    });
+
+    // Create the required charts
     const yearOrders = document.getElementById('this-year-orders');
     const ordersChart = new Chart(yearOrders, {
         type: 'bar',
@@ -28,6 +41,21 @@ document.addEventListener('DOMContentLoaded', function () {
                 data: ordersNumber,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            }]
+        }
+    });
+
+    const totalMoney = document.getElementById('this-year-money');
+    const moneyChart = new Chart(totalMoney, {
+        type: 'bar',
+        data: {
+            labels: months,
+            datasets: [{
+                label: 'Fatturato',
+                data: moneyGained,
+                backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                borderColor: 'rgba(255, 159, 64, 1)',
                 borderWidth: 1,
             }]
         }
--- resources/views/admin/orders/stats.blade.php
+++ resources/views/admin/orders/stats.blade.php
@@ -4,6 +4,7 @@
 <div class="container bg-white">
     {{-- Canvas is needed in order to render the chart --}}
     <canvas id="this-year-orders" width="400" height="150"></canvas>
+    <canvas id="this-year-money" width="400" height="150"></canvas>
     
     <script>
         // Send the JSON to JavaScript
