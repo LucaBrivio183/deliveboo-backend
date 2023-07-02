f87fbd1bbc673498ad12570a267ae919c9c0a29a
eugeniarossi <eugenia.rossi@outlook.com>
2023-07-02 22:05:57 +0200

    feat: favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="shortcut icon" href="{{ asset('/favicon.png') }}">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
